94d3f01a5cb207e2e827352094d4302f756d4275
chiazam <54377599+chiazam@users.noreply.github.com>
2022-10-15 04:31:59 +0100

    Update signup.js

--- api/signup.js
+++ api/signup.js
@@ -2,7 +2,7 @@
 
 const signupfunc = (req, res) => {
 
-    // PMAK-6349eded4c38d627f47429a2-4b7fb0897e89baaa566414a2fbf3a9be58
+    
     // res.statusCode = 200;
     // console.log(req.body);
     // console.log(req.params);

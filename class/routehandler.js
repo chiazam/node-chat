d888d39aca0a571f2598b2f53e72881ecba4ad16
chiazam <54377599+chiazam@users.noreply.github.com>
2022-10-15 04:29:50 +0100

    ghnj

--- class/routehandler.js
+++ class/routehandler.js
@@ -1,19 +1,60 @@
 const http         = require('http');
 const Router       = require('router');
+var compression  = require('compression');
 const finalhandler = require('finalhandler')
 const bodyParser   = require('body-parser');
+const multer  = require('multer');
+const storage = multer.diskStorage({   
+  destination: function(req, file, cb) { 
+     cb(null, './files');    
+  },
+  filename: (req, file, cb) => {
+    const ext = file.mimetype.split("/")[1];
+    cb(null, `${file.fieldname}-${Date.now()}.${ext}`);
+  }
+});
+const formdata = multer({
+  storage: storage,
+  limits : {fileSize : 10000000},
+  fileFilter:(req, file, cb) => {    
+    // Allowed ext
+     const filetypes = /jpeg|jpg|png|jiff|gif/;
+   // Check ext
+    const extname =  filetypes.test(file.originalname.toLowerCase());
+   // Check mime
+   const mimetype = filetypes.test(file.mimetype);
+  
+   if(mimetype && extname){
+       return cb(null,true);
+   } else {
+       cb('Error: Images Only!');
+   }
+  }
+});
 
-exports.routeconn = (routearr)=>{
+exports.routeropt = {mergeParams: true};
 
-  let router = Router()
+exports.routeconn = (routeopt,routearr)=>{
 
-  router.use(bodyParser.json())
+  let router = Router(routeopt);
+
+  router.use(bodyParser.json());
+  router.use(bodyParser.urlencoded({ extended: false }));
+  router.use(compression());
 
   for (i=0;i<routearr.length;i++){
 
     let routearrnow = routearr[i];
 
+    if(routearrnow.hasOwnProperty('uploads')){
+
+      router[routearrnow.method](routearrnow.path,formdata.array(routearrnow.uploads.name,routearrnow.uploads.num), routearrnow.func);
+
+    }else{
+
     router[routearrnow.method](routearrnow.path, routearrnow.func);
+
+    }
 
   }
     
--- index.js
+++ index.js
@@ -1,19 +1,9 @@
 const routemod = require('./class/routehandler.js');
 
-const routes = [{
-    method:'get',
-    path:"/",
-    func:(req, res) => {
+let signup = require('./api/signup.js');
 
-        // console.log(req);
+const routes = [
+    signup.signuproute
+];
 
-        res.statusCode = 200;
-
-        res.setHeader('Content-Type', 'text/plain; charset=utf-8');
-
-        res.end('Hello World! \n');
-    
-    }
-}];
-
-routemod.routeconn(routes);
+routemod.routeconn(routemod.routeropt,routes);

0504d61f24df89dfdd2cd78b39cf220fc99d8ff7
chiazam <54377599+chiazam@users.noreply.github.com>
2022-10-17 02:04:24 +0100

    ghj

--- api/signup.js
+++ api/signup.js
@@ -1,25 +1,61 @@
 routemod = require('../class/routehandler.js');
 
+const signupnow = (body, files) => {
+
+    if (body.hasOwnproperty('name') && body.hasOwnproperty('user') && body.hasOwnproperty('pass')) {
+
+        if (body.name.length === 0) {
+
+            return { err: "Input your Fullname!" };
+
+        } else if (body.user.length === 0) {
+
+            return { err: "Input your Username!" };
+
+        } else if (body.pass.length === 0) {
+
+            return { err: "Input your Password!" };
+
+        } else if (files.length === 0) {
+
+            return { err: "Input your Picture!" };
+
+        } else {
+
+        }
+
+    } else {
+
+        return { err: "Permission Denied!" };
+
+    }
+
+};
+
 const signupfunc = (req, res) => {
 
-    
     // res.statusCode = 200;
     // console.log(req.body);
     // console.log(req.params);
 
     req.params = routemod.routequery(req).query;
-
     res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify([req.body,req.params,req.files]));
+
+    let api = signupnow(req.body, req.files);
+
+    res.end(JSON.stringify({
+        api: api,
+        req: [req.body, req.params, req.files]
+    }));
 
 };
 
 exports.signuproute = {
-    method:'post',
-    path:"/api/signup/",
-    func:signupfunc,
-    uploads:{
-        name:"pix",
-        num:1
+    method: 'post',
+    path: "/api/signup/",
+    func: signupfunc,
+    uploads: {
+        name: "pix",
+        num: 1
     }
 };
